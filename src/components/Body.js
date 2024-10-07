6c5a0d92acf0321e218ba26980508333fb028d1e
bhanu mukkamala <bmukkamala68@gmail.com>
2024-10-07 22:47:58 +0530

    Made the button top restaurants work

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,9 +2,6 @@ import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
 import { useState } from "react";
 
-
-
-
 const Body = () =>{
  
   const [listOfRestaurants,setlistOfRestaurants] = useState(resList);
@@ -14,15 +11,15 @@ const Body = () =>{
     <div className="filter">
       <button className="filter-btn" onClick={()=>{
         const filteredList=listOfRestaurants.filter(
-        (res) => res.avgRating > 4.2
+        (res) => res.data.avgRating > 4.2
         )
         setlistOfRestaurants(filteredList);
       }}>Top Rated Restaurants</button>
     </div>
     <div className="res-container">
      {
-      resList.map((restaurant) => {
-       return(  <RestaurantCard key={restaurant.id} resData={restaurant}/>);
+      listOfRestaurants.map((restaurant) => {
+       return(  <RestaurantCard key={restaurant.data.id} resData={restaurant}/>);
       })
      }
     </div>
