09b68e51b7fecb0883803a67d15cbec208f294bb
bhanu mukkamala <bmukkamala68@gmail.com>
2024-10-14 23:32:37 +0530

    New changes

--- src/app.js
+++ src/app.js
@@ -2,15 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-
-
-
+import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
+import Error from "./components/Error";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 const AppLayout = () =>(
   <div className="app">
     <Header/>
-    <Body/>
+    <Outlet/>
   </div>
 );
+const appRouter = createBrowserRouter([
+  {path:"/"
+    ,element:<AppLayout/>,
+    children:[
+      {path:"/",
+        element:<Body/>
+      },
+      {
+    path:"/about",
+    element:<About/>
+  },
+  {
+    path:"/contact",
+    element:<Contact/>
+  }
+]
+    ,errorElement:<Error/>
+  }
+  
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<RouterProvider router={appRouter}/>);
